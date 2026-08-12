b4965ce52555da7cda6d8b4daa47498bd2271e24
Pau27-rai <paulocolmenares27@gmail.com>
2026-08-12 11:21:50 +0800

    added lorem php

---
+++ Lab2/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident soluta at illum, culpa asperiores a est reiciendis cum eum cupiditate, quam, totam praesentium ratione dolore aperiam placeat et esse iure?</P>
+</body>
+</html>
