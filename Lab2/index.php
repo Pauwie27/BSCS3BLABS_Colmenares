8acf9ee5e58a8f3c354478bb6ce79613fdf18046
Pau27-rai <paulocolmenares27@gmail.com>
2026-08-14 15:31:15 +0800

    adding my lab 2 assignment

--- Lab2/index.php
+++ Lab2/index.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
+
+<?php 
+$pageTitle = "First Page";
+$pageHeader = "Main Page - Home";
+ ?>
+
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident soluta at illum, culpa asperiores a est reiciendis cum eum cupiditate, quam, totam praesentium ratione dolore aperiam placeat et esse iure?</P>
-</body>
+
+        <head>
+
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo $pageTitle ?></title>
+
+        </head>
+        <body>
+            
+            <h1><?php echo $pageHeader; ?> </h1>
+            <p>WELCOME TO MY PERSONAL PORTFOLIO!</p>
+            <h4>INFO</h4>
+            <p>This page includes my name, birthday and current occupation</p>
+            <p>Name: Paulo Colmenares <br> Birthday: October 27, 2004 <br> Occupation: College Student/Working Student Assitant </p>
+
+            <img src="images/susdog.gif" width="500" height="500">
+            
+
+
+            <h2>Navigation</h2>
+            <ul>
+                <li><a href="Page2.php">Page 2 <br></a></li>
+                <li><a href="Page3.php">Page 3</a></li>
+            </ul>
+
+        </body>
 </html>
